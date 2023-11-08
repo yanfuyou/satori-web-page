34437fc0f0a2893e53f7217fed6390009988416d
cat_y <1839224665@qq.com>
2023-11-08 23:22:58 +0800

    烟花test

--- src/components/user/user-home.vue
+++ src/components/user/user-home.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-      {{ 'this is main' }}
+    <!-- 我的宠物 -->
+      <el-row :gutter="20" class="pet">
+      {{ 'my pet' }}
+      </el-row>
+      <!-- 内容信息 -->
+      <el-row :gutter="20" class="history">
+      {{ 'my wontent' }}
+      </el-row>
     </div>
   </template>
   
@@ -8,5 +15,7 @@
   import { ref } from "vue";
   </script>
   <style scoped>
-
+  .pet{
+    height: 200px;
+  }
   </style>
